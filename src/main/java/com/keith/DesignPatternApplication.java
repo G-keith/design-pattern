c8008a33c4ef499806b8f3a5f06f1ba306951e60
keith <G-keith>
2020-05-16 13:43:13 +0800

    Initial commit

---
+++ src/main/java/com/keith/DesignPatternApplication.java
@@ -0,0 +1,18 @@
+package com.keith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2020-05-15
+ **/
+@SpringBootApplication
+public class DesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternApplication.class, args);
+    }
+
+}
