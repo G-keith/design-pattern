5899f4a908d9a712a03e686f9c36bd43cfa4193a
keith <G-keith>
2020-05-19 15:43:58 +0800

    单例模式-饿汉式(静态代码块)

--- src/main/java/com/keith/pattern/single/SingleTest01.java
+++ src/main/java/com/keith/pattern/single/SingleTest01.java
@@ -9,12 +9,23 @@ package com.keith.pattern.single;
 public class SingleTest01 {
     public static void main(String[] args) {
         Singleton01 singleton01=Singleton01.getInstance();
-        Singleton01 singleton02=Singleton01.getInstance();
-        System.out.println(singleton01==singleton02);
+        Singleton01 singleton011=Singleton01.getInstance();
+        System.out.println(singleton01==singleton011);
         System.out.println("singleton01 hashCode："+singleton01.hashCode());
+        System.out.println("singleton01 hashCode："+singleton011.hashCode());
+
+        System.out.println("==================================================");
+        Singleton02 singleton02=Singleton02.getInstance();
+        Singleton02 singleton021=Singleton02.getInstance();
+        System.out.println(singleton02==singleton021);
         System.out.println("singleton01 hashCode："+singleton02.hashCode());
+        System.out.println("singleton01 hashCode："+singleton021.hashCode());
     }
 }
+
+/**
+ * 饿汉式（静态常量）
+ */
 class Singleton01{
 
     /**
@@ -33,6 +44,31 @@ class Singleton01{
      * 向外暴露一个静态的公共方法
      */
     public static Singleton01 getInstance(){
+        return instance;
+    }
+}
+
+class Singleton02{
+
+    /**
+     * 构造器私有化 防止外部new
+     */
+    private Singleton02(){
+
+    }
+    /**
+     * 类的内部创建对象
+     */
+    private static Singleton02 instance;
+
+    static {
+        instance=new Singleton02();
+    }
+
+    /**
+     * 向外暴露一个静态的公共方法
+     */
+    public static Singleton02 getInstance(){
         return instance;
     }
 }
