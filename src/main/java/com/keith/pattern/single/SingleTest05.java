cff1b46a847d1fd98e9832caeb990c414804cd6b
keith <G-keith>
2020-05-19 16:27:17 +0800

    单例模式-枚举类

---
+++ src/main/java/com/keith/pattern/single/SingleTest05.java
@@ -0,0 +1,19 @@
+package com.keith.pattern.single;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2020-05-19
+ **/
+public class SingleTest05 {
+}
+enum Singleton08{
+    /**
+     *
+     */
+    INSTANCE;
+
+    public void method(){
+
+    }
+}
